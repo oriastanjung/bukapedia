dcb4665f4d4cc1e0a7b3316d22a28b40a24e3ed1
O. Riastanjung <oriastan999@gmail.com>
2022-11-19 01:23:38 +0700

    finish calculate : but minimal accuracy

--- src/pages/Cart/Sum.jsx
+++ src/pages/Cart/Sum.jsx
@@ -5,23 +5,23 @@ import axios from "axios";
 const Sum = (props) => {
   const data = props.data;
   const [total, setTotal] = useState(0);
-
+  let tempTotal = 0
   const [dataProduct, setDataProduct] = useState([]);
   const tempData = [];
-  async function getProduct(id) {
+  async function getProduct(id,qty) {
     try {
       const res = await axios.get(`https://fakestoreapi.com/products/${id}`);
-      return res.data;
+      return {data :res.data, quantity : qty};
     } catch (error) {
       console.log(error);
     }
   }
 
   const fixedPromiseData = [];
-
+  
   useEffect(() => {
     for (let i = 0; i < data.length; i++) {
-      let temp = getProduct(data[i].productId);
+      let temp = getProduct(data[i].productId, data[i].quantity);
       tempData.push(temp);
     }
     Promise.all(tempData).then((res) => {
@@ -31,39 +31,34 @@ const Sum = (props) => {
       // setDataProduct(res)
     });    
     console.log("fixedata >> ", fixedPromiseData);
+    
     setDataProduct(fixedPromiseData);
+
   }, []);
 
+  const countTotal = () => {
+    dataProduct.map((item, key) => {
+      if(key == 0){
+        item.map((product) => {
+          console.log("product >>> ", product);
+          tempTotal = tempTotal + product.quantity* product.data.price
+        })
+      }
+    })
+    return tempTotal
   
-  let [temporTotal, setTemporTotal] = useState(0)
-
+  }
   useEffect(() => {
-    console.log("data product >>> ", dataProduct)
-
-      dataProduct.map((item, key) => {
-        if (key == 0){
-          item.map((product) => {
-            data.map((productData) => {
-              if (product.id === productData.productId){
-                temporTotal = temporTotal + (product.price * productData.quantity)
-              }
-            })
-          })
-        }
-      })
-      console.log("tempor total " , temporTotal);
-      setTemporTotal(temporTotal)
-
+    if(dataProduct){
+      setTotal(countTotal() + total)
+    }
   },[dataProduct])
+  
+
   return (
     <div className="flex justify-between font-bold text-xl px-32 py-16 ">
       <div>Jumlah</div>
-      {
-        temporTotal != 0 && <h1>{temporTotal}</h1> 
-      }
-      {
-        temporTotal == 0 && <p>perlu render lagi di save baru nampil</p>
-      }
+      {total}
     </div>
   );
 };
